7b3f61548f19f2a6fb7b0ba55d7ce9ff69b533a3
John H Wallace <john.wallace@hamnavoepartners.com>
2022-11-17 13:32:05 +0000

    add use-credentials so works with ngrok

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   get '/service-worker.js', to: 'service_workers/workers#index'
-  get '/manifest.json', to: 'service_workers/manifests#index'  
+  get '/manifest.json', to: 'service_workers/manifests#index'
   root 'welcome#index'
 end
