ff57bc477fc2aa7214fbe65b9291abb09b3de9d1
John H Wallace <john.wallace@hamnavoepartners.com>
2022-11-17 16:53:01 +0000

    fix width on small for users, order users correctly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    @users = User.all
+    @users = User.all.order(:name)
   end
 
   # GET /users/1 or /users/1.json
