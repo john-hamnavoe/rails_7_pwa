8879c0c385a3c31365f2b494cbda8f0f0398c7e0
John H Wallace <john.wallace@hamnavoepartners.com>
2022-11-17 12:27:51 +0000

    register service worker

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,10 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/service-worker.js', { scope: './' })
+                         .then(function(registration) {
+                           console.log('[Companion]', 'Service worker registered!')
+                           console.log(registration)
+                         })
+}
